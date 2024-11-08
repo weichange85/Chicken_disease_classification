0d99fbc181b4f6a7251dcf39d305eec459fe0cd4
Wei Chang <wei.chang85@gmail.com>
2024-11-08 13:35:39 +1000

    bugging thru ingestion

--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
 CONFIG_FILE_PATH = Path("config/config.yaml")
-PARAMs_FILE_PATH = Path("params.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
 
--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -8,15 +8,27 @@ from ensure import ensure_annotations
 from box import ConfigBox
 from typing import Any
 import base64
+from pathlib import Path
 
 @ensure_annotations
-def read_yaml_file(file_path) -> ConfigBox:
-    """reads yamk and returns ConfigBox type"""
+def read_yaml_file(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox type
+    """
     try:
-        with open(file_path, 'r') as file:
-            content = yaml.safe_load(file)
-            logger.info(f"yaml file: {file_path} loaded successfully")
-            return content
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
     except Exception as e:
@@ -38,3 +50,18 @@ def save_json(out_filepath, data:dict):
         json.dump(data, file, indent=4)
 
     logger.info(f"json file saved at: {out_filepath}")
+
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """get size in kb.
+
+    Args:
+        path (Path): path to the file.
+
+    Returns:
+        str: size in kB
+    """
+
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
