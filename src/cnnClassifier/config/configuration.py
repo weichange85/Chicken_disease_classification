809e0a6b1c8c37d328f32deaf6007420f918d080
Wei Chang <wei.chang85@gmail.com>
2024-11-13 00:12:46 +1000

    finish 02 stage

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,6 @@
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml_file, create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig
+from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
 
 class ConfigurationManager:
     def __init__(
@@ -26,4 +26,23 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        params = self.params.prepare_base_model
+
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=config.root_dir,
+            base_model_path=config.base_model_path,
+            updated_base_model_path=config.updated_base_model_path,
+            params_image_size = params.IMAGE_SIZE,
+            params_learning_rate = params.LEARNING_RATE,
+            params_include_top = params.INCLUDE_TOP,
+            params_weights = params.WEIGHTS,
+            params_classes = params.CLASSES
+        )
+
+        return prepare_base_model_config
     
